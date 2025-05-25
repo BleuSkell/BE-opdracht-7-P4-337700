7310e417b084f88e7a89f5c286104d27018c59f3
BleuSkell <kyasowi1@gmail.com>
2025-05-25 16:04:41 +0200

    putting data into the index

--- app/controllers/Instructeurs.php
+++ app/controllers/Instructeurs.php
@@ -1,13 +1,37 @@
 <?php
 
 class Instructeurs extends BaseController
-{
+{   
+    private $instructeurModel;
+
+    public function __construct()
+    {
+        $this->instructeurModel = $this->model('InstructeurModel');
+    }
 
     public function index()
     {
         $data = [
-            'title' => 'Dit is de instructeurspagina'
+            'title' => 'Dit is de instructeurspagina',
+            'message' => NULL,
+            'messageColor' => NULL,
+            'messageVisibility' => 'none',
+            'dataRows' => NULL
         ];
+
+        $result = $this->instructeurModel->getAllInstructeurs();
+
+        if (is_null($result)) {
+            // Fout afhandelen
+            $data['message'] = "Er is een fout opgetreden in de database";
+            $data['messageColor'] = "danger";
+            $data['messageVisibility'] = "flex";
+            $data['dataRows'] = NULL;
+
+            header('Refresh:3; url=' . URLROOT . '/Homepages/index');
+        } else {
+            $data['dataRows'] = $result;
+        }
 
         $this->view('instructeurs/index', $data);
     }
---
+++ app/models/InstructeurModel.php
@@ -0,0 +1,31 @@
+<?php
+
+class InstructeurModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        /**
+         * Maak een nieuw database object die verbinding maakt met de 
+         * MySQL server
+         */
+        $this->db = new Database();
+    }
+
+    public function getAllInstructeurs()
+    {
+        try {
+            $sql = "CALL spGetAllInstructeurs()";
+
+            $this->db->query($sql);
+
+            return $this->db->resultSet();
+        } catch (Exception $e) {
+            /**
+             * Log de error in de functie logger()
+             */
+            logger(__LINE__, __METHOD__, __FILE__, $e->getMessage());  
+        }
+    }
+}
